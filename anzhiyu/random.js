523cfe5529b013048b83659d457d259b9378c76a
luckgold <luckgold5888@gmail.com>
2024-08-27 03:02:26 +0800

    Site updated: 2024-08-27 03:02:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/27/hello-world/"];function toRandomPost(){
+var posts=["2024/08/27/标题：探索数字货币世界：如何选择最佳交易平台并获得10000元奖励/","2024/08/27/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
